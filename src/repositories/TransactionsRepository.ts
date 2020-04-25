e8893577cf286e052b68d635387669477a3b122f
alevieiramoreira <alessandra-vieira-moreira@hotmail.com>
2020-04-24 21:18:04 -0300

    adiciona projeto

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,34 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === 'income') {
+          accumulator.income += Number(transaction.value);
+        }
+        if (transaction.type === 'outcome') {
+          accumulator.outcome += Number(transaction.value);
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 }
 
